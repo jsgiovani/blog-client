a6675d9b274a8244641a895de87650cca8575170
jsgiovani <=>
2024-01-24 18:20:16 -0500

    create pages and install react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,42 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import Logout from "./pages/Logout";
+import Projects from "./pages/projects/Projects";
+
 export default function App() {
   return (
-    <h1>
-      Hello world!
-    </h1>
+    <BrowserRouter>
+    <Routes>
+        <Route
+            path = "/"
+            element = {<Home/>}
+        />
+        <Route
+            path = "/about"
+            element = {<About/>}
+        />
+        <Route
+            path = "/dashboard"
+            element = {<Dashboard/>}
+        />
+        <Route
+            path = "/login"
+            element = {<Login/>}
+        />
+        <Route
+            path = "/logout"
+            element = {<Logout/>}
+        />
+
+        <Route
+            path = "/projects"
+            element = {<Projects/>}
+        />
+
+    </Routes>
+    </BrowserRouter>
   )
 }
