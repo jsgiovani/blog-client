344700538ab1647df149440b2e03bd37e58cbea6
jsgiovani <=>
2024-01-26 00:01:54 -0500

    implement login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,62 @@
-import { Button, Label, TextInput } from 'flowbite-react'
-import React from 'react'
-import { Link } from 'react-router-dom'
+import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import axiosConnection from '../config/axios';
 
 const Login = () => {
+
+    const navegate = useNavigate();
+
+    const [user, setUser] = useState({
+        email:'',
+        password:''
+    });
+
+
+    const [alert, setAlert] = useState({status:false});
+    const [loading, setLoading] = useState(false);
+
+
+
+
+    const handleChange = (e)=>{
+        setUser({...user, [e.target.id]:e.target.value.trim()});
+    };
+
+
+    const handleSubmit = async (e)=>{
+        e.preventDefault();
+
+
+        if (Object.values(user).includes('')) {
+            return setAlert({message:'All fields are required', status:true});
+        }
+
+
+        try {
+            setLoading(true);
+            setAlert({status:false});
+            const {data} = await axiosConnection.post('/api/auth/login', user);
+
+            if (data.success ===false) {
+                setAlert({message:data.message, status:true});
+                return setLoading(false);
+            }
+
+            setLoading(false);
+            navegate('/');
+
+
+        } catch (error) {
+            setAlert({message:error.response.data.message, status:true});
+            setLoading(false);
+        }
+
+
+
+    };
+
+
   return (
     <main className='min-h-screen mt-20'>
 
@@ -17,7 +71,10 @@ const Login = () => {
             </div>
 
             <div>
-                <form className='space-y-4'>
+
+       
+            
+                <form className='space-y-4' onSubmit={handleSubmit}>
 
 
                     <div>
@@ -25,12 +82,14 @@ const Login = () => {
                             value='Your email'
                             htmlFor='email'
 
+
                         />
                         
                         <TextInput
                             type='email'
                             placeholder='Your email'
                             id='email'
+                            onChange={handleChange}
                         />
                     </div>
 
@@ -45,6 +104,7 @@ const Login = () => {
                             type='password'
                             placeholder='Password'
                             id='password'
+                            onChange={handleChange}
                         />
                     </div>
 
@@ -52,18 +112,25 @@ const Login = () => {
                         gradientDuoTone={'purpleToPink'}
                         type='submit'
                         className='w-full'
+                        disabled = {loading}
+
                     >
-                        Register
+                        {loading ? <Spinner size={'sm'}  />:'Login'}
                     </Button>
 
 
                 </form>
 
                 <div className='flex items-center gap-1 text-sm mt-4'>
-                    <span>Do not have an account?</span>
+                    <span>Dont have an account?</span>
                     <Link className='text-blue-500' to="/register">Register</Link>
 
                 </div>
+
+
+                {alert.status && (
+                    <Alert className='mt-4' color={'failure'}>{alert.message}</Alert>
+                )}
 
 
             </div>
