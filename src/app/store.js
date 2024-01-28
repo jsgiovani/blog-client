d5f7d8a1c6665e2e6ecd69c588ad1ea4f613054f
jsgiovani <=>
2024-01-28 10:12:34 -0500

    add redux-persist to save user data to local storage

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,33 @@
-import { configureStore } from '@reduxjs/toolkit'
-import userReducer from '../features/user/userSlice'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import userReducer from '../features/user/userSlice';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'
+
+
+
+const rootReducer = combineReducers({
+  user:userReducer,
+});
+
+
+const rootPersistConfig = {
+  key: 'root',
+  storage,
+  version:1
+};
+
+
+
+const persistedReducer = persistReducer(rootPersistConfig, rootReducer);
+
 
 export const store = configureStore({
-  reducer: {
-    user:userReducer
-  },
-})
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+  getDefaultMiddleware({
+    serializableCheck: false,
+  }),
+});
+
+
+export const persistor = persistStore(store);
